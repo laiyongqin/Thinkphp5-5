d0967827d511e3425742cb7ce7ba83b8cc87db63
guizhiquan <1834537260@qq.com>
2018-03-15 18:18:28 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Thinkphp5-
+# Thinkphp5
 thinkphp5网站开发，包括小程序api数据接口
---
+++ application/index/controller/Index.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\index\controller;
+
+use app\index\model\Index as IndexModel;
+use think\Db;
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        $list  = IndexModel::all();
+        $list2 = Db::name('data')
+            ->where('id', 1)
+            ->find();
+        $this->assign('list', $list);
+        return $this->fetch();
+    }
+
+    public function hello($name = 'World')
+    {
+        return 'hello,' . $name;
+    }
+}
