3f6990014163e25fe25ac66778c894bbe4dafe22
guizhiquan <1834537260@qq.com>
2018-10-27 09:14:53 +0800

    区分小写

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -15,7 +15,7 @@ class Index extends Controller
             ->find();
         $this->assign('list', $list);
         $this->assign('meta_title', '小姐姐小姐姐');
-        return $this->fetch('Tricks/index');
+        return $this->fetch('tricks/index');
     }
 
     public function hello($name = 'World')
